3e4e8cbcf4efad81a635e685e61211508e90942f
Merkul8 <max.merkulov.00@mail.ru>
2024-01-24 13:30:11 +0300

    delete message

--- market_auth/views.py
+++ market_auth/views.py
@@ -6,9 +6,8 @@ from market.tasks import (
     )
 from .forms import CustomerRegisterForm, CustomerLoginForm
 from django.contrib.auth import login, logout
-from django.contrib import messages
 from cart.models import Cart
-from .models import Customer, Role
+from .models import Role
 
 def user_login(request):
     if request.method == 'POST':
@@ -32,11 +31,8 @@ def register(request):
             user = form.save()
             Cart.objects.create(customer_id=user)
             login(request, user)
-            messages.success(request, 'Успешная регистрация')
             send_mail_before_sing_in.delay(form.cleaned_data['email'], form.cleaned_data['username'])
             return redirect('home')
-        else:
-            messages.error(request, 'Ошибка регистрации')
     else:
         form = CustomerRegisterForm()
     return render(request, 'market_auth/register.html', {'form': form})
@@ -52,11 +48,8 @@ def register_for_seller(request):
             user.save()
             Cart.objects.create(customer_id=user)
             login(request, user)
-            messages.success(request, 'Успешная регистрация')
             send_mail_for_sellers.delay(form.cleaned_data['email'], form.cleaned_data['username'])
             return redirect('home')
-        else:
-            messages.error(request, 'Ошибка регистрации')
     else:
         form = CustomerRegisterForm()
     return render(request, 'market_auth/register_for_seller.html', {'form': form})
